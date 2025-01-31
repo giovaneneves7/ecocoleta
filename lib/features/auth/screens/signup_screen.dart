74ff4dad78cf5c290560663975a8421b2b3124ff
giovaneneves7 <autornekoyasha@gmail.com>
2025-01-31 11:23:38 -0300

    🌱 Feat(auth): Create signup screen

---
+++ lib/features/auth/screens/signup_screen.dart
@@ -0,0 +1,18 @@
+import 'package:ecocoleta/helper/route_helper.dart';
+import 'package:ecocoleta/util/images.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:get/get.dart';
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
