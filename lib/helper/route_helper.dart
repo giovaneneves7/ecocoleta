99b73f4f9422c7f46e023b479c5a76315b910678
giovaneneves7 <autornekoyasha@gmail.com>
2025-01-31 11:22:19 -0300

    ⚡ Update: Add signup screen route

--- lib/helper/route_helper.dart
+++ lib/helper/route_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:ecocoleta/features/auth/screens/login_screen.dart';
+import 'package:ecocoleta/features/auth/screens/signup_screen.dart';
 import 'package:ecocoleta/features/home/screens/home_screen.dart';
 import 'package:get/get.dart';
 
@@ -13,14 +14,17 @@ class RouteHelper{
 
   static const String login = '/login';
   static const String home = '/home';
+  static const String signup = '/signup';
 
   static String getLoginScreen() => login;
   static String getHomeScreen() => home;
+  static String getSignupScreen() => signup;
 
   // Registro de rotas [ Adicionar todas as rotas do app aqui ] 
   static List<GetPage> routes = [
     GetPage(name: login, page: () => LoginScreen()),
     GetPage(name: home, page: () => HomeScreen()),
+    GetPage(name: signup, page: () => SignupScreen())
   ];
 
 }
