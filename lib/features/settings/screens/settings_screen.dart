b3efd61e9a991bd323f8657e8ec0d2bb41674697
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-03 12:53:10 -0300

    🌱 feat (settings): Create settings screen

---
+++ lib/features/settings/screens/settings_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/**
+* @author Giovane Neves
+* @since v0.0.1
+*/
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+
+}
