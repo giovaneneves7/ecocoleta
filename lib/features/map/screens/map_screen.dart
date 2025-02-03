db810eb6f503c328e8fec0f89a65b0c5103ea658
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-03 12:45:31 -0300

    🌱 feat (map): Create map screen

---
+++ lib/features/map/screens/map_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/**
+* @author Giovane Neves
+* @since v0.0.1
+*/
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+
+}
