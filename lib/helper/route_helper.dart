5d8038ee87652703b75210511453089cf8db819a
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-03 12:54:21 -0300

    ⚡  Update: Add settings and map routes

--- lib/helper/route_helper.dart
+++ lib/helper/route_helper.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:ecocoleta/features/auth/screens/login_screen.dart';
 import 'package:ecocoleta/features/auth/screens/signup_screen.dart';
 import 'package:ecocoleta/features/home/screens/home_screen.dart';
+import 'package:ecocoleta/features/map/map_screen.dart';
 import 'package:get/get.dart';
 
 /**
@@ -14,16 +15,19 @@ class RouteHelper{
 
   static const String login = '/login';
   static const String home = '/home';
+  static const String map = '/map';
   static const String signup = '/signup';
 
   static String getLoginScreen() => login;
   static String getHomeScreen() => home;
+  static String getMapScreen() => map;
   static String getSignupScreen() => signup;
 
   // Registro de rotas [ Adicionar todas as rotas do app aqui ] 
   static List<GetPage> routes = [
     GetPage(name: login, page: () => LoginScreen()),
     GetPage(name: home, page: () => HomeScreen()),
+    GetPage(name: map, page: () => MapScreen()),
     GetPage(name: signup, page: () => SignupScreen())
   ];
 
