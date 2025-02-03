38cc1d0e58d9941b10d233ff3861746b1fddfac0
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-03 13:44:51 -0300

    🪲 Fix: Solve image path error

--- lib/util/images.dart
+++ lib/util/images.dart
@@ -2,7 +2,7 @@ class Images{
 
 	static const logoDark = 'assets/images/logo_dark.png';
 	static const pontoColetaMock1 = 'assets/images/praca_do_feijao.jpeg';
-	static const pontoColetaMock2 = 'assets/images/rua_1_de_janeiro.jpeg';
+	static const pontoColetaMock2 = 'assets/images/rua_1_de_janeiro.jpg';
 	static const promotionalBanner = 'assets/images/promotional_banner.png';
 	static const splashBackground = 'assets/images/splash_background.png'; 
 }
